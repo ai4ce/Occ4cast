381e41972433e25b2f29c3ebc777d3b3a11f5b7e
Xinhao Liu <gavinliu_hm@hotmail.com>
2023-07-25 05:01:33 -0400

    add reproducibility

--- baselines/4docc/train.py
+++ baselines/4docc/train.py
@@ -219,7 +219,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # Reproducibility
     np.random.seed(0)
-    torch.random.manual_seed(0)
+    torch.manual_seed(0)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
     train(args)
