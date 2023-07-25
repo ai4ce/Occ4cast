672ad90b851150df08cdce26b0e4360f11df5ff5
Xinhao Liu <gavinliu_hm@hotmail.com>
2023-07-24 21:18:25 -0400

    fix dataloader bug

--- baselines/4docc/data/lyft.py
+++ baselines/4docc/data/lyft.py
@@ -52,7 +52,7 @@ class LyftDataset(Dataset):
         label = data['label'][self.p_post:]
         invalid = data['invalid'][self.p_post:]
         input_tensor = torch.tensor(input, dtype=torch.float32)
-        label_tensor = torch.tensor(label, dtype=torch.float32)
+        label_tensor = torch.tensor(label, dtype=torch.int)
         invalid_tensor = torch.from_numpy(invalid)
         return input_tensor, label_tensor, invalid_tensor
 
